5f55d0a6b880eec684830845d1efb2bc40a37b03
kazisabjan <kazisabjan@gmail.com>
2024-10-18 10:03:57 +0600

    first commit

---
+++ EvenOdd.js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+      console.log(number + " is even.");
+    } else {
+      console.log(number + " is odd.");
+    }
+  }
+   
+  //Exmple usage
+  checkEvenOrOdd(5); 
+  checkEvenOrOdd(15);
